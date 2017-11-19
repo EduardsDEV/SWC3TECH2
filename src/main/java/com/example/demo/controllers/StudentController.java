f46047a38e4698ced1c57fa67bc487f822287e95
eduards <edua0193@stud.kea.dk>
2017-11-19 15:27:51 +0100

    Student view added, course info page added

--- src/main/java/com/example/demo/controllers/StudentController.java
+++ src/main/java/com/example/demo/controllers/StudentController.java
@@ -1,6 +1,44 @@
 package com.example.demo.controllers;
+import com.example.demo.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StudentController {
+
+    private final CourseRepository courseRepository;
+
+
+    @Autowired
+    public StudentController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+
+//    @GetMapping("/student/showcourses")
+//    public String loadAddCoursePage() {
+//
+//        return "viewcourses";
+//    }
+
+    @GetMapping("/student/showcourses")
+    public ModelAndView showCourses(){
+        ModelAndView mv = new ModelAndView("viewcourses");
+        mv.getModel().put("courseList", courseRepository.findAll());
+        return mv;
+    }
+
+
+    @GetMapping("/student/courseinfo")
+    public ModelAndView viewCourseInfo(
+                        @RequestParam(name = "id", defaultValue = "0")
+                                long id){
+        ModelAndView mv = new ModelAndView("courseinfo");
+        mv.getModel().put("course", courseRepository.getCourseById(id));
+        return mv;
+    }
+
 }
