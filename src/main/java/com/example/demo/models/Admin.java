93283451627e78b5f2f1bde03c67cd676d862ade
eduards <edua0193@stud.kea.dk>
2017-12-01 09:03:12 +0100

    admin model changed

--- src/main/java/com/example/demo/models/Admin.java
+++ src/main/java/com/example/demo/models/Admin.java
@@ -18,4 +18,36 @@ public class Admin {
     private String lastName;
     private String password;
 
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
